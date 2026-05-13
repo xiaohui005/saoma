441275a5c9c2a21afa77bf92a00d6425e2308179
xiaohui005 <dfsafswerewerwsa@gmail.com>
2026-05-13 10:28:22 +0530

    统一 Android JVM 编译目标

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,17 @@ android {
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
